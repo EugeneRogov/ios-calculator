ef0d6dfe34ddd37e1a8546a4b7472f5554870e8e
Eugene Rogov <res@habarlink.com>
2017-10-11 15:27:37 +0300

    added View Controllers for calculators

--- Multi Calculator/ViewController.swift
+++ Multi Calculator/ViewController.swift
@@ -11,11 +11,19 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var btnNavCalcSimple: UIButton!
+    @IBOutlet weak var btnNavCalcVat: UIButton!
+    @IBOutlet weak var btnNavCalcDate: UIButton!
+    @IBOutlet weak var btnNavCalcPercent: UIButton!
+    @IBOutlet weak var btnNavCalcLoan: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         btnNavCalcSimple.setTitle(NSLocalizedString("navigation_menu_simple", comment: ""), for: UIControlState.normal)
+        btnNavCalcVat.setTitle(NSLocalizedString("navigation_menu_vat", comment: ""), for: UIControlState.normal)
+        btnNavCalcDate.setTitle(NSLocalizedString("navigation_menu_date", comment: ""), for: UIControlState.normal)
+        btnNavCalcPercent.setTitle(NSLocalizedString("navigation_menu_percent", comment: ""), for: UIControlState.normal)
+        btnNavCalcLoan.setTitle(NSLocalizedString("navigation_menu_loan", comment: ""), for: UIControlState.normal)
     }
 
     override func didReceiveMemoryWarning() {
