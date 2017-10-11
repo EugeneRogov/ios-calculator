e76dd764fd58752ea0d2709bd3dfce3f47e143b9
Eugene Rogov <res@habarlink.com>
2017-10-11 13:06:07 +0300

    Initial Commit

---
+++ Multi Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Multi Calculator
+//
+//  Created by Евгений Рогов on 11.10.17.
+//  Copyright © 2017 XDPI Lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
