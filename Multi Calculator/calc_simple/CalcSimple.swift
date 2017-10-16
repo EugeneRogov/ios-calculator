55434d9bd18a6c4fdd928ab5ddea6bfb6227e491
Eugene Rogov <res@habarlink.com>
2017-10-16 17:17:08 +0300

    CalcSimple added view fields

--- Multi Calculator/calc_simple/CalcSimple.swift
+++ Multi Calculator/calc_simple/CalcSimple.swift
@@ -11,7 +11,32 @@ import UIKit
 class CalcSimple: UIViewController {
     
     @IBOutlet weak var btnMc: UIButton!
-    
+    @IBOutlet weak var btnMminus: UIButton!
+    @IBOutlet weak var bntMplus: UIButton!
+    @IBOutlet weak var btnMr: UIButton!
+    @IBOutlet weak var btnAc: UIButton!
+    @IBOutlet weak var btnLeftBracket: UIButton!
+    @IBOutlet weak var btnRightBracket: UIButton!
+    @IBOutlet weak var btnDivision: UIButton!
+    @IBOutlet weak var btnRoot: UIButton!
+    @IBOutlet weak var btnPercent: UIButton!
+    @IBOutlet weak var btnPlusMinus: UIButton!
+    @IBOutlet weak var btnMultiply: UIButton!
+    @IBOutlet weak var btn7: UIButton!
+    @IBOutlet weak var btn8: UIButton!
+    @IBOutlet weak var btn9: UIButton!
+    @IBOutlet weak var btnMinus: UIButton!
+    @IBOutlet weak var btn4: UIButton!
+    @IBOutlet weak var btn5: UIButton!
+    @IBOutlet weak var btn6: UIButton!
+    @IBOutlet weak var btnPlus: UIButton!
+    @IBOutlet weak var btn1: UIButton!
+    @IBOutlet weak var btn2: UIButton!
+    @IBOutlet weak var btn3: UIButton!
+    @IBOutlet weak var btn0: UIButton!
+    @IBOutlet weak var btnDot: UIButton!
+    @IBOutlet weak var btnDel: UIButton!
+    @IBOutlet weak var btnEqual: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
