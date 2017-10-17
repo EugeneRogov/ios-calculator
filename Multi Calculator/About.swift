61dd85af95b0ca5a42ee4f350ab6b0fa3c8b5efb
Eugene Rogov <res@habarlink.com>
2017-10-17 19:43:47 +0300

    About added send email; added version number

--- Multi Calculator/About.swift
+++ Multi Calculator/About.swift
@@ -7,16 +7,55 @@
 //
 
 import UIKit
+import MessageUI
 
-class About: UIViewController {
+class About: UIViewController, MFMailComposeViewControllerDelegate {
     
     @IBOutlet weak var tvText: UITextView!
+    @IBOutlet weak var btnSendEmail: UIButton!
     @IBOutlet weak var ivIcon: UIImageView!
+    @IBOutlet weak var appName: UILabel!
+    @IBOutlet weak var version: UILabel!
+    
+    @IBAction func btnSenEmail(_ sender: Any) {
+        let mailComposerViewController = configureMailController()
+        if MFMailComposeViewController.canSendMail() {
+            self.present(mailComposerViewController, animated: true, completion: nil)
+        } else {
+            showMailError()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        initViews()
+    }
+    
+    fileprivate func initViews() {
+        appName.text = NSLocalizedString("app_name", comment: "")
+        let versionNumber = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String
+        version.text = NSLocalizedString("version", comment: "") + " " + versionNumber
+    }
+    
+    func configureMailController() -> MFMailComposeViewController {
+        let mailComposerVC = MFMailComposeViewController()
+        mailComposerVC.mailComposeDelegate = self
+        mailComposerVC.setToRecipients(["xdpilab@gmail.com"])
+        mailComposerVC.setSubject("Debug info, Calculator")
+//        mailComposerVC.setMessageBody("How are you", isHTML: false)
         
-        
+        return mailComposerVC
+    }
+    
+    func showMailError() {
+        let sendMailErrorAlert = UIAlertController(title: "Could not send email", message: "could not send email", preferredStyle: .alert)
+        let dismiss = UIAlertAction(title: "OK", style: .default, handler: nil)
+        sendMailErrorAlert.addAction(dismiss)
+        self.present(sendMailErrorAlert, animated: true, completion: nil)
+    }
+    
+    func  mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true, completion: nil)
     }
     
 }
