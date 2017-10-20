f093460b90833e296fc5e739ed451b67ad97ec38
Eugene Rogov <res@habarlink.com>
2017-10-20 15:57:07 +0300

    added common action buttons

--- Multi Calculator/calc_percent/CalcPercent.swift
+++ Multi Calculator/calc_percent/CalcPercent.swift
@@ -10,29 +10,29 @@ import UIKit
 
 class CalcPercent: UIViewController {
     
-    @IBOutlet weak var tvTitle1: UILabel!
-    @IBOutlet weak var tvPercent1: UILabel!
-    @IBOutlet weak var tvNumber1: UILabel!
-    @IBOutlet weak var etPercent1: UITextField!
-    @IBOutlet weak var etNumber1: UITextField!
-    
-    @IBOutlet weak var tvTitle2: UILabel!
-    @IBOutlet weak var tvNumber2: UILabel!
-    @IBOutlet weak var tvNumber2_: UILabel!
-    @IBOutlet weak var etNumber2: UITextField!
-    @IBOutlet weak var etNumber2_: UITextField!
-    
-    @IBOutlet weak var tvTitle3: UILabel!
-    @IBOutlet weak var tvPercent3: UILabel!
-    @IBOutlet weak var tvNumber3: UILabel!
-    @IBOutlet weak var etPercent3: UITextField!
-    @IBOutlet weak var etNumber3: UITextField!
-    
-    @IBOutlet weak var tvTitle4: UILabel!
-    @IBOutlet weak var tvPercent4: UILabel!
-    @IBOutlet weak var tvNumber4: UILabel!
-    @IBOutlet weak var etPercent4: UITextField!
-    @IBOutlet weak var etNumber4: UITextField!
+//    @IBOutlet weak var tvTitle1: UILabel!
+//    @IBOutlet weak var tvPercent1: UILabel!
+//    @IBOutlet weak var tvNumber1: UILabel!
+//    @IBOutlet weak var etPercent1: UITextField!
+//    @IBOutlet weak var etNumber1: UITextField!
+//
+//    @IBOutlet weak var tvTitle2: UILabel!
+//    @IBOutlet weak var tvNumber2: UILabel!
+//    @IBOutlet weak var tvNumber2_: UILabel!
+//    @IBOutlet weak var etNumber2: UITextField!
+//    @IBOutlet weak var etNumber2_: UITextField!
+//
+//    @IBOutlet weak var tvTitle3: UILabel!
+//    @IBOutlet weak var tvPercent3: UILabel!
+//    @IBOutlet weak var tvNumber3: UILabel!
+//    @IBOutlet weak var etPercent3: UITextField!
+//    @IBOutlet weak var etNumber3: UITextField!
+//
+//    @IBOutlet weak var tvTitle4: UILabel!
+//    @IBOutlet weak var tvPercent4: UILabel!
+//    @IBOutlet weak var tvNumber4: UILabel!
+//    @IBOutlet weak var etPercent4: UITextField!
+//    @IBOutlet weak var etNumber4: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,21 +41,21 @@ class CalcPercent: UIViewController {
     }
     
     fileprivate func initViews() {
-        tvTitle1.text = NSLocalizedString("how_much_is", comment: "").uppercased()
-        tvPercent1.text = NSLocalizedString("percent", comment: "")
-        tvNumber1.text = NSLocalizedString("number", comment: "")
-        
-        tvTitle2.text = NSLocalizedString("what_percent_of_the_number_of", comment: "").uppercased()
-        tvNumber2.text = NSLocalizedString("number", comment: "")
-        tvNumber2_.text = NSLocalizedString("number", comment: "")
-        
-        tvTitle3.text = NSLocalizedString("add", comment: "").uppercased()
-        tvPercent3.text = NSLocalizedString("percent", comment: "")
-        tvNumber3.text = NSLocalizedString("number", comment: "")
-        
-        tvTitle4.text = NSLocalizedString("subtract", comment: "").uppercased()
-        tvPercent4.text = NSLocalizedString("percent", comment: "")
-        tvNumber4.text = NSLocalizedString("number", comment: "")
+//        tvTitle1.text = NSLocalizedString("how_much_is", comment: "").uppercased()
+//        tvPercent1.text = NSLocalizedString("percent", comment: "")
+//        tvNumber1.text = NSLocalizedString("number", comment: "")
+//        
+//        tvTitle2.text = NSLocalizedString("what_percent_of_the_number_of", comment: "").uppercased()
+//        tvNumber2.text = NSLocalizedString("number", comment: "")
+//        tvNumber2_.text = NSLocalizedString("number", comment: "")
+//        
+//        tvTitle3.text = NSLocalizedString("add", comment: "").uppercased()
+//        tvPercent3.text = NSLocalizedString("percent", comment: "")
+//        tvNumber3.text = NSLocalizedString("number", comment: "")
+//        
+//        tvTitle4.text = NSLocalizedString("subtract", comment: "").uppercased()
+//        tvPercent4.text = NSLocalizedString("percent", comment: "")
+//        tvNumber4.text = NSLocalizedString("number", comment: "")
         
     }
     
