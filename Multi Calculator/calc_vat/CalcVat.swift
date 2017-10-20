06934d52c366dcc46c2c9666b4c1557c5b5a58d3
Eugene Rogov <res@habarlink.com>
2017-10-20 13:38:44 +0300

    CalcVat start recreate

--- Multi Calculator/calc_vat/CalcVat.swift
+++ Multi Calculator/calc_vat/CalcVat.swift
@@ -12,20 +12,20 @@ class CalcVat: UIViewController {
     
 //    @IBOutlet weak var tvRateOfVat: UILabel!
 //    @IBOutlet weak var tvAmount: UILabel!
-    @IBOutlet weak var etRateOfVat: UITextField!
-    @IBOutlet weak var etAmount: UITextField!
+//    @IBOutlet weak var etRateOfVat: UITextField!
+//    @IBOutlet weak var etAmount: UITextField!
     
-    @IBOutlet weak var tvAddVat: UILabel!
-    @IBOutlet weak var tvAddSumVat: UILabel!
+//    @IBOutlet weak var tvAddVat: UILabel!
+//    @IBOutlet weak var tvAddSumVat: UILabel!
 //    @IBOutlet weak var tvAddSumWithVat: UILabel!
 //    @IBOutlet weak var tvAddSumVatResult: UILabel!
 //    @IBOutlet weak var tvAddSumWithVatResult: UILabel!
     
 //    @IBOutlet weak var tvExcludeVat: UILabel!
-    @IBOutlet weak var tvExcludeSumVat: UILabel!
-    @IBOutlet weak var tvExcludeSumWithVat: UILabel!
-    @IBOutlet weak var tvExcludeSumVatResult: UILabel!
-    @IBOutlet weak var tvExcludeSumWithVatResult: UILabel!
+//    @IBOutlet weak var tvExcludeSumVat: UILabel!
+//    @IBOutlet weak var tvExcludeSumWithVat: UILabel!
+//    @IBOutlet weak var tvExcludeSumVatResult: UILabel!
+//    @IBOutlet weak var tvExcludeSumWithVatResult: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,20 +37,20 @@ class CalcVat: UIViewController {
 //        tvRateOfVat.text = NSLocalizedString("rate_of_vat", comment: "")
 //        tvAmount.text = NSLocalizedString("amount", comment: "")
         
-        tvAddVat.text = NSLocalizedString("add_vat", comment: "")
-        tvAddSumVat.text = NSLocalizedString("sum_vat", comment: "")
+//        tvAddVat.text = NSLocalizedString("add_vat", comment: "")
+//        tvAddSumVat.text = NSLocalizedString("sum_vat", comment: "")
 //        tvAddSumWithVat.text = NSLocalizedString("sum_with_vat", comment: "")
         
 //        tvExcludeVat.text = NSLocalizedString("exclude_vat", comment: "")
-        tvExcludeSumVat.text = NSLocalizedString("sum_vat", comment: "")
-        tvExcludeSumWithVat.text = NSLocalizedString("sum_without_vat", comment: "")
+//        tvExcludeSumVat.text = NSLocalizedString("sum_vat", comment: "")
+//        tvExcludeSumWithVat.text = NSLocalizedString("sum_without_vat", comment: "")
         
         
-        tvAddVat.text = tvAddVat.text?.uppercased()
+//        tvAddVat.text = tvAddVat.text?.uppercased()
 //        tvExcludeVat.text = tvExcludeVat.text?.uppercased()
         
         
-        etRateOfVat.placeholder = "dfdfdffd"
+//        etRateOfVat.placeholder = "dfdfdffd"
         
     }
     
