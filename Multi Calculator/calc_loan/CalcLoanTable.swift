b784cf04b22c8807377a83f60579e069479632cd
Eugene Rogov <res@habarlink.com>
2017-10-23 12:55:23 +0300

    CalcLoan start adding views

---
+++ Multi Calculator/calc_loan/CalcLoanTable.swift
@@ -0,0 +1,28 @@
+//
+//  CalcLoanTable.swift
+//  Multi Calculator
+//
+//  Created by Евгений Рогов on 23.10.17.
+//  Copyright © 2017 XDPI Lab. All rights reserved.
+//
+
+import UIKit
+
+class CalcLoanTable: UITableViewController {
+    
+    //    @IBOutlet weak var btnAnnuity: UIButton!
+    //    @IBOutlet weak var btnDifferentiated: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initViews()
+        
+    }
+    
+    fileprivate func initViews() {
+        //        btnAnnuity.setTitle(NSLocalizedString("calc_loan_annuity", comment: "").uppercased(), for: UIControlState.normal)
+        //        btnDifferentiated.setTitle(NSLocalizedString("calc_loan_differentiated", comment: "").uppercased(), for: UIControlState.normal)
+        
+    }
+    
+}
