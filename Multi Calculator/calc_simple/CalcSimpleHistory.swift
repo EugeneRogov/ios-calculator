1b6e28b06278e596faf6d22167f78488ee7be389
Eugene Rogov <res@habarlink.com>
2017-10-24 11:03:51 +0300

    ref

--- Multi Calculator/calc_simple/CalcSimpleHistory.swift
+++ Multi Calculator/calc_simple/CalcSimpleHistory.swift
@@ -1,14 +1,14 @@
 //
-//  SettingsCalcSimple.swift
+//  CalcSimpleHistory.swift
 //  Multi Calculator
 //
-//  Created by Евгений Рогов on 16.10.17.
+//  Created by Евгений Рогов on 24.10.17.
 //  Copyright © 2017 XDPI Lab. All rights reserved.
 //
 
 import UIKit
 
-class SettingsCalcSimple: UITableViewController {
+class CalcSimpleHistory: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ Multi Calculator/calc_simple/CalcSimpleSettings.swift
@@ -0,0 +1,19 @@
+//
+//  SettingsCalcSimple.swift
+//  Multi Calculator
+//
+//  Created by Евгений Рогов on 16.10.17.
+//  Copyright © 2017 XDPI Lab. All rights reserved.
+//
+
+import UIKit
+
+class CalcSimpleSettings: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+}
